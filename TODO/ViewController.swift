fd7195efb421f1d4121f22bf8dd0e7ee8d93c2f3
TT8 <tomasztacka@gmail.com>
2019-08-04 22:17:25 +0200

    initial commit

---
+++ TODO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by TT8 on 04/08/2019.
+//  Copyright © 2019 TT8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
